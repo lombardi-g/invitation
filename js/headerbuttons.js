c42220eefec6d210d5a5bdf867ae1e4f2fe66000
lombardi-g <gabriellomba@gmail.com>
2025-10-25 11:14:45 -0300

    header buttons navigation

---
+++ js/headerbuttons.js
@@ -0,0 +1,29 @@
+let introHeight = document.getElementById('intro').offsetHeight + document.getElementById('date-information').offsetHeight + document.getElementById('header').offsetHeight;
+let dressHeight = document.getElementById('RSVP').offsetHeight + introHeight;
+let locationHeight = document.getElementById('dress-code').offsetHeight + dressHeight;
+
+document.getElementById('RSVP-button').addEventListener('click',function(){
+  window.scrollTo({
+    top:introHeight,
+    behavior: 'smooth'});
+  // console.log(introHeight)
+});
+
+document.getElementById('dress-button').addEventListener('click',function(){
+  window.scrollTo({
+    top:dressHeight,
+    behavior: 'smooth'});
+});
+
+document.getElementById('location-button').addEventListener('click',function(){
+  window.scrollTo({
+    top:locationHeight,
+    behavior: 'smooth'});
+});
+
+document.getElementById('gifts-button').addEventListener('click', function() {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth'
+  })
+});

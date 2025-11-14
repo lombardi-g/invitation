4d524cbb06de321e1732c3e88b214ec063fc7f5f
Gabriel Lombardi <gabriellomba@gmail.com>
2025-11-14 11:24:23 -0300

    Edit gifts scroll function

--- js/headerbuttons.js
+++ js/headerbuttons.js
@@ -1,6 +1,7 @@
 let introHeight = document.getElementById('intro').offsetHeight + document.getElementById('date-information').offsetHeight + document.getElementById('header').offsetHeight;
 let dressHeight = document.getElementById('RSVP').offsetHeight + introHeight;
 let locationHeight = document.getElementById('dress-code').offsetHeight + dressHeight;
+let giftsHeight = document.getElementById('gifts-menu').offsetHeight + locationHeight;
 
 document.getElementById('RSVP-button').addEventListener('click',function(){
   window.scrollTo({
@@ -21,9 +22,16 @@ document.getElementById('location-button').addEventListener('click',function(){
     behavior: 'smooth'});
 });
 
-document.getElementById('gifts-button').addEventListener('click', function() {
+/*document.getElementById('gifts-button').addEventListener('click', function() {
   window.scrollTo({
     top: document.body.scrollHeight,
     behavior: 'smooth'
   })
+});*/
+
+document.getElementById('gifts-button').addEventListener('click',function(){
+  window.scrollTo({
+    top:giftsHeight,
+    behavior:'smooth'
+  });
 });
